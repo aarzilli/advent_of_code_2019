0af71493f1d9f0a442143cc7ea3976f01e1213be
aarzilli <alessandro.arzilli@gmail.com>
2019-12-01 06:21:27 +0100

    day 1 cleanup

--- 01.go
+++ 01.go
@@ -13,43 +13,11 @@ func must(err error) {
 	}
 }
 
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
-}
-
-// splits a string, trims spaces on every element
-func splitandclean(in, sep string, n int) []string {
-	v := strings.SplitN(in, sep, n)
-	for i := range v {
-		v[i] = strings.TrimSpace(v[i])
-	}
-	return v
-}
-
 // convert string to integer
 func atoi(in string) int {
 	n, err := strconv.Atoi(in)
 	must(err)
 	return n
-}
-
-// convert vector of strings to integer
-func vatoi(in []string) []int {
-	r := make([]int, len(in))
-	for i := range in {
-		var err error
-		r[i], err = strconv.Atoi(in[i])
-		must(err)
-	}
-	return r
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
 }
 
 func mass(n int) int {
@@ -72,20 +40,20 @@ func totalmass(n int) int {
 const part2 = true
 
 func main() {
-	fmt.Printf("hello\n")
 	buf, err := ioutil.ReadFile("01.txt")
 	must(err)
-	var r int
+	var part1, part2 int
 	for _, line := range strings.Split(string(buf), "\n") {
 		line = strings.TrimSpace(line)
 		if line == "" {
 			continue
 		}
-		if !part2 {
-			r += mass(atoi(line))
-		} else {
-			r += totalmass(atoi(line))
-		}
+		part1 += mass(atoi(line))
+		part2 += totalmass(atoi(line))
 	}
-	fmt.Printf("result: %d\n", r)
+	fmt.Printf("PART 1: %d\n", part1)
+	fmt.Printf("PART 2: %d\n", part2)
 }
+
+// Part 1: 3229279
+// Part 2: 4841054
