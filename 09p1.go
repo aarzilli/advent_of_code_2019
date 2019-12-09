bff36dc70e56838825140c51ef98894336675342
aarzilli <alessandro.arzilli@gmail.com>
2019-12-09 08:32:04 +0100

    day 9

---
+++ 09p1.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// returns x without the last character
+func nolast(x string) string {
+	return x[:len(x)-1]
+}
+
+// splits a string, trims spaces on every element
+func splitandclean(in, sep string, n int) []string {
+	v := strings.SplitN(in, sep, n)
+	for i := range v {
+		v[i] = strings.TrimSpace(v[i])
+	}
+	return v
+}
+
+// convert string to integer
+func atoi(in string) int {
+	n, err := strconv.Atoi(in)
+	must(err)
+	return n
+}
+
+// convert vector of strings to integer
+func vatoi(in []string) []int {
+	r := make([]int, len(in))
+	for i := range in {
+		var err error
+		r[i], err = strconv.Atoi(in[i])
+		must(err)
+	}
+	return r
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+type Opcode struct {
+	Len  int
+	Name string
+}
+
+var Opcodes = map[int]Opcode{
+	1:  {4, "ADD"},
+	2:  {4, "MUL"},
+	3:  {2, "INPUT"},
+	4:  {2, "OUTPUT"},
+	5:  {3, "JNZ"},
+	6:  {3, "JZ"},
+	7:  {4, "LT"},
+	8:  {4, "EQ"},
+	9:  {2, "REL"},
+	99: {1, "END"},
+}
+
+const TRACECPU = false
+
+func prettyInstr(p []int, pc, mode, opcode int, a []int) {
+	modev := make([]int, 3)
+	modev[0] = mode % 10
+	modev[1] = (mode / 10) % 10
+	modev[2] = (mode / 100) % 10
+	symop := fmt.Sprintf("? %d", opcode+mode*100)
+	if oc, ok := Opcodes[opcode]; ok {
+		symop = oc.Name
+	}
+	fmt.Printf("%04d\tmode=%03d %s\t", pc, mode, symop)
+	for i := range a {
+		switch modev[i] {
+		case 0:
+			if a[i] < len(p) {
+				fmt.Printf(" [%d]=%d", a[i], p[a[i]])
+			} else {
+				fmt.Printf(" [%d]", a[i])
+			}
+		case 1:
+			fmt.Printf(" %d", a[i])
+		case 2:
+			fmt.Printf(" [%d+REL]", a[i])
+		}
+	}
+	fmt.Printf("\n")
+}
+
+func cpu(p []int, input int) []int {
+	p = copyprog(p)
+	mem := make(map[int]int)
+	pc := 0
+	relativeBase := 0
+
+	modev := make([]int, 3)
+
+evalLoop:
+	for pc < len(p) {
+		opcode := p[pc]
+		mode := opcode / 100
+		opcode = opcode % 100
+
+		modev[0] = mode % 10
+		modev[1] = (mode / 10) % 10
+		modev[2] = (mode / 100) % 10
+
+		n := Opcodes[opcode].Len
+		a := p[pc : pc+n][1:]
+
+		arg := func(n int) int {
+			var addr int
+			switch modev[n] {
+			case 0:
+				addr = a[n]
+			case 1:
+				return a[n]
+			case 2:
+				addr = a[n] + relativeBase
+			default:
+				panic("wtf")
+			}
+			if addr < len(p) {
+				return p[addr]
+			} else {
+				return mem[addr]
+			}
+			
+		}
+		
+		save := func(n, out int) {
+			addr := 0
+			switch modev[n] {
+			case 0:
+				addr = a[n]
+			case 1:
+				panic("wtf")
+			case 2:
+				addr = a[n] + relativeBase
+			default:
+				panic("wtf")
+			}
+			if addr < len(p) {
+				p[addr] = out
+			} else {
+				mem[addr] = out
+			}
+		}
+
+		jumped := false
+
+		if TRACECPU {
+			prettyInstr(p, pc, mode, opcode, a)
+		}
+
+		switch opcode {
+		case 1:
+			save(2, arg(0) + arg(1))
+		case 2:
+			save(2, arg(0) * arg(1))
+
+		case 3: // input
+			save(0, input)
+		case 4: // output
+			fmt.Printf("OUT: %d\n", arg(0))
+
+		case 5:
+			if arg(0) != 0 {
+				pc = arg(1)
+				jumped = true
+			}
+
+		case 6:
+			if arg(0) == 0 {
+				pc = arg(1)
+				jumped = true
+			}
+
+		case 7:
+			if arg(0) < arg(1) {
+				save(2, 1)
+			} else {
+				save(2, 0)
+			}
+		case 8:
+			if arg(0) == arg(1) {
+				save(2, 1)
+			} else {
+				save(2, 0)
+			}
+		case 9:
+			relativeBase += arg(0)
+
+		case 99:
+			break evalLoop
+		}
+		if !jumped {
+			pc += n
+		}
+	}
+
+	return p
+}
+
+func copyprog(p []int) []int {
+	q := make([]int, len(p))
+	copy(q, p)
+	return q
+}
+
+func readprog(path string) []int {
+	buf, err := ioutil.ReadFile(path)
+	must(err)
+	var p []int
+	for _, line := range strings.Split(string(buf), "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		p = append(p, vatoi(splitandclean(line, ",", -1))...)
+	}
+	return p
+}
+
+func main() {
+	p := readprog("09.txt")
+	cpu(p, 1)
+}
